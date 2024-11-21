659f4f70590e568ce15c336df89e3e9e90e2744a
Sparshlohana <sparsh.lohana@gmail.com>
2024-11-21 13:06:35 +0530

    adsense added

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
       </body>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3840129051176908"
+        crossOrigin="anonymous"></script>
     </html>
   );
 }
